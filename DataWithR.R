735ebb1cc372ccf677dcf1d6642dc928f51dc0c3
Anjani Sowmya <bollapragada@arizona.edu>
2023-09-04 21:35:26 +0000

    Installed packages

---
+++ DataWithR.R
@@ -0,0 +1,8 @@
+library(pacman)
+
+p_load(dlookr,
+       DMwR2, # Data Mining with R functions
+       GGally, # Pair-wise plots using ggplot2
+       Hmisc, # Data analysis 
+       palmerpenguins, # Alternative to the Iris dataset
+       tidyverse) # Data wrangling, manipulation, visualization
